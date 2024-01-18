1f75c43968a4bb0c2e080ed79ca8765fe124b511
Fabian Baena <fabian.baena@samanagroup.com>
2024-01-18 09:15:49 -0500

    review code

--- docs/examples/query_for_namespaces.py
+++ docs/examples/query_for_namespaces.py
@@ -1,5 +1,6 @@
 import xml.etree.ElementTree as ET
 from sammwr.protocol import WRProtocol
+from sammwr.wmi import WMIQuery
 import os
 import datetime
 
@@ -22,25 +23,48 @@ def print_class(res):
 		print(i.attrib['NAME'])
 
 
-
 selector=[{"@Name": "__namespace", "#text": "Root"}]
 enum=c.enumerate(resource_uri, selector=selector)
-c=WRProtocol(endpoint=endpoint, username=username, password=password)
-resource_uri="http://schemas.microsoft.com/wbem/wsman/1/wmi/root/__Namespace"
-enum=c.enumerate(resource_uri)
-ec=get_enumeration_context(enum)
-while ec is not None:
-	pull=c.pull(resource_uri, ec)
-	pull
-	ec=get_enumeration_context(pull)
 
-resource_uri="http://schemas.dmtf.org/wbem/cim-xml/2/cim-schema/2/*"
-enum=c.enumerate(resource_uri, selector=[{"@Name": "__namespace", "#text": "root"}])
-ec=get_enumeration_context(enum)
-while ec is not None:
-	pull=c.pull(resource_uri, ec)
-	print_class(pull)
-	ec=get_enumeration_context(pull)
+c=WRProtocol(endpoint=endpoint, username=username, password=password)
+def get_namespaces(c, root="Root"):
+	''' List all namespaces '''
+	resource_uri="http://schemas.microsoft.com/wbem/wsman/1/wmi/%s/__Namespace" % root
+	enum=c.enumerate(resource_uri)
+	ec=get_enumeration_context(enum)
+	r=None
+	while ec is not None:
+		pull=c.pull(resource_uri, ec)
+		if r is None:
+			r = ET.fromstring(pull)
+			items = r.find(".//{http://schemas.xmlsoap.org/ws/2004/09/enumeration}Items")
+		else:
+			temp = ET.fromstring(pull)
+			temp_items = temp.find(".//{http://schemas.xmlsoap.org/ws/2004/09/enumeration}Items")
+			for i in temp_items:
+				items.append(i)
+		ec=get_enumeration_context(pull)
+	return [ x.text for x in r.findall(".//{http://schemas.microsoft.com/wbem/wsman/1/wmi/root/__NAMESPACE}Name")]
+
+# List all classes in a namespace
+def get_classes(c, root="root"):
+	ns = { "e": "http://schemas.xmlsoap.org/ws/2004/09/enumeration" }
+	resource_uri="http://schemas.dmtf.org/wbem/cim-xml/2/cim-schema/2/*"
+	enum=c.enumerate(resource_uri, selector=[{"@Name": "__cimnamespace", "#text": root}])
+	ec=get_enumeration_context(enum)
+	r=None
+	while ec is not None:
+		pull=c.pull(resource_uri, ec)
+		if r is None:
+			r = ET.fromstring(pull)
+			items = r.find(".//e:Items", ns)
+		else:
+			temp = ET.fromstring(pull)
+			temp_items = temp.find(".//e:Items", ns)
+			for i in temp_items:
+				items.append(i)
+		ec=get_enumeration_context(pull)
+	return [ x.attrib.get('NAME') for x in items ]
 
 class CIMClient:
 	resource_uri="http://schemas.dmtf.org/wbem/cim-xml/2/cim-schema/2/*"
@@ -168,3 +192,65 @@ class CIMClass:
 		if key in self._properties:
 			return self._properties[key]
 		raise AttributeError(key)
+
+from sammwr.wmi import WMIQuery
+# DNS Statistics
+dns_class_names = [
+	"Win32_PerfRawData_DNS_DNS"
+]
+
+# AD Statistics
+ad_class_names = [
+	"Win32_PerfRawData_DirectoryServices_DirectoryServices",
+	"Win32_PerfRawData_NTDS_NTDS",
+	"Win32_PerfRawData_Lsa_SecuritySystemWideStatistics"
+]
+
+# Certification authority
+adcs_class_names = [
+	"Win32_PerfRawData_CertSvc_CertificationAuthority"
+]
+
+# ADFS
+adfs_class_names = [
+	"Win32_PerfRawData_GenevaServerProvider_ADFS"
+]
+
+
+# Cache
+cache_class_names = [
+	"Win32_PerfRawData_PerfOS_Cache"
+]
+
+# DFS
+dfs_class_names = [
+	"Win32_PerfRawData_DFSNServerService_DFSNamespace",
+	"Win32_PerfRawData_DFSNServerService_DFSNamespaceServiceAPIRequests",
+	"Win32_PerfRawData_DFSNServerService_DFSNamespaceServiceReferrals",
+	"Win32_PerfRawData_dfsr_DFSReplicatedFolders",
+	"Win32_PerfRawData_dfsr_DFSReplicationConnections",
+	"Win32_PerfRawData_dfsr_DFSReplicationServiceVolumes"
+]
+
+# DHCP
+dhcp_class_names = [
+	"Win32_PerfRawData_dhcpserver_DHCPServer",
+	"Win32_PerfRawData_dhcpserver_DHCPServerv6"
+]
+
+# Web Server
+web_class_names = [
+	"Win32_PerfRawData_W3SVC_WebService"
+]
+
+# TS
+ts_class_names = [
+	"Win32_PerfRawData_TermService_TerminalServicesSession",
+	"Win32_PerfRawData_LocalSessionManager_TerminalServices"
+]
+
+class_name="Win32_LoggedOnUser"
+q=WMIQuery(class_name=class_name, namespace="root/cimv2", protocol=c)
+data = list(q)
+len(data)
+data
